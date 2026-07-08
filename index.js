54c143509e383c438b7a74da11da54b17edb0df7
Eva Fridman <fridman.1999@mail.ru>
2026-07-08 19:54:12 +0300

    corrections

--- index.js
+++ index.js
@@ -3,47 +3,51 @@ const track = document.querySelector("#track");
 const ball = document.querySelector("#ball");
 const fps = document.querySelector("#fps");
 
+const speed = 0.2;
 let x = 0;
+let lastTime = null;
+let fpsCount = 0;
+let fpsTimer = null;
+
+function step (currentTime) {
+    if (!lastTime) {
+        lastTime = currentTime;
+        fpsTimer = currentTime;
+        requestAnimationFrame(step);
+        return;
+    }
+
+    fpsCount++;
+
+    if (currentTime - fpsTimer >= 1000) {
+        fps.textContent = `FPS: ${fpsCount}`;
+        fpsCount = 0;
+        fpsTimer = currentTime;
+    }
+    
+    const deltaTime = currentTime - lastTime;
+    lastTime = currentTime;
+
+    x += speed * deltaTime;
+    ball.style.transform = `translateX(${x}px)`;
+
+    const maxDistance = track.offsetWidth - ball.offsetWidth;
+    
+    if (x < maxDistance) {
+        requestAnimationFrame(step);
+    } else {
+        runBtn.disabled = false;
+    }
+}
+
 runBtn.addEventListener("click", () => {
     runBtn.disabled = true;
     x = 0;
-    const speed = 0.2;
-    let lastTime = null;
+    lastTime = null;
+    fpsCount = 0;
+    fpsTimer = null;
 
-    let fpsCount = 0;
-    let fpsTimer = null;
-
-    function step (currentTime) {
-        if (!lastTime) {
-            lastTime = currentTime;
-            fpsTimer = currentTime;
-            requestAnimationFrame(step);
-            return;
-        }
-
-        fpsCount++
-
-        if (currentTime - fpsTimer >= 1000) {
-            fps.textContent = `FPS: ${fpsCount}`;
-            fpsCount = 0;
-            fpsTimer = currentTime;
-        }
-        
-        const deltaTime = currentTime - lastTime;
-        lastTime = currentTime
-
-        x += speed * deltaTime;
-
-        ball.style.transform = `translateX(${x}px)`;
-
-        const maxDistance = track.offsetWidth - ball.offsetWidth;
-        if (x < maxDistance) {
-            requestAnimationFrame(step);
-        } else {
-            runBtn.disabled = false;
-        };
-    };
     requestAnimationFrame(step);
 });
-// Анимировать transform, а не left, дешевле для браузера, потому что так запускается только composite, а не layout.
 
+// Анимировать transform, а не left, дешевле для браузера, потому что так запускается только composite, а не layout.
