ceaa8f3ba4f0bde322d14cfa51f4d4703cd75580
Eva Fridman <fridman.1999@mail.ru>
2026-07-08 19:15:17 +0300

    release 3

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const runBtn = document.querySelector("#run");
 const track = document.querySelector("#track");
 const ball = document.querySelector("#ball");
+const fps = document.querySelector("#fps");
 
 let x = 0;
 runBtn.addEventListener("click", () => {
     runBtn.disabled = true;
     x = 0;
-    const speed = 0.3;
+    const speed = 0.2;
     let lastTime = null;
+
+    let fpsCount = 0;
+    let fpsTimer = null;
 
     function step (currentTime) {
         if (!lastTime) {
             lastTime = currentTime;
+            fpsTimer = currentTime;
             requestAnimationFrame(step);
             return;
+        }
+
+        fpsCount++
+
+        if (currentTime - fpsTimer >= 1000) {
+            fps.textContent = `FPS: ${fpsCount}`;
+            fpsCount = 0;
+            fpsTimer = currentTime;
         }
         
         const deltaTime = currentTime - lastTime;
