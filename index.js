ad9a358d592fbbf524a81a5ccd360ba958d557dc
Eva Fridman <fridman.1999@mail.ru>
2026-07-08 19:07:55 +0300

    release 2

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ let x = 0;
 runBtn.addEventListener("click", () => {
     runBtn.disabled = true;
     x = 0;
+    const speed = 0.3;
+    let lastTime = null;
 
-    function step () {
-        x += 1;
+    function step (currentTime) {
+        if (!lastTime) {
+            lastTime = currentTime;
+            requestAnimationFrame(step);
+            return;
+        }
+        
+        const deltaTime = currentTime - lastTime;
+        lastTime = currentTime
+
+        x += speed * deltaTime;
+
         ball.style.transform = `translateX(${x}px)`;
+
         const maxDistance = track.offsetWidth - ball.offsetWidth;
         if (x < maxDistance) {
             requestAnimationFrame(step);
@@ -19,5 +32,5 @@ runBtn.addEventListener("click", () => {
     };
     requestAnimationFrame(step);
 });
-
 // Анимировать transform, а не left, дешевле для браузера, потому что так запускается только composite, а не layout.
+
